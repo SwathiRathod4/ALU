2ea696b4ac648ffb7b8aa7efdf6c80dd5953b6c8
SwathiRathod4 <swathirathod16062003@gmail.com>
2025-08-01 15:33:39 +0530

    Update alu_top.sv

--- DOC/doc1/alu_top.sv
+++ DOC/doc1/alu_top.sv
@@ -1,41 +1,3 @@
- /*`include "design.sv"
-`include "alu_package.sv"
-`include "alu_interface.sv"
-module top();
-  import alu_pkg::*;
-  bit CLK,RST,CE;
-  parameter int N = 8;
-  parameter int M = 4;
-  alu_interface intf(CLK,RST);
-
-    alu_test dut=new(intf.MON,intf.DRV,intf.REF);
-
-  initial
-    begin
-      CLK = 0;
-      forever #5 CLK=~CLK;
-    end
-
-  initial begin
-    @(posedge CLK)
-    CE = 1'b1;
-    RST=1;
-    repeat(3) @(posedge CLK)
-      RST=0;
-
-      end
-
-
-    //dut instaintaion
-  ALU_DESIGN  #(  .DW(N) , .CW(M) ) dut1(.INP_VALID(intf.INP_VALID),.OPA(intf.OPA),.OPB(intf.OPB),.CIN(intf.CIN),.CLK(intf.CLK),.RST(intf.RST),.CMD(intf.CMD),.CE(intf.CE),.MODE(intf.MODE),.COUT(intf.COUT),.OFLOW(intf.OFLOW),.RES(intf.RES),.G(intf.G),.E(intf.E),.L(intf.L),.ERR(intf.ERR));
-
-    initial begin
-    dut.run();
-#250     $finish;
-    end
-
-endmodule*/
-
 
 
 //`include "alu_test.sv"
@@ -92,18 +54,18 @@ module top();
 
   initial begin
 
-//    tb  = new(intf.MON, intf.DRV, intf.REF);
-//
-  //  tb1 = new(intf.MON, intf.DRV, intf.REF);
+    tb  = new(intf.MON, intf.DRV, intf.REF);
+
+    tb1 = new(intf.MON, intf.DRV, intf.REF);
 
 
-    //tb2 = new(intf.MON, intf.DRV, intf.REF);
+    tb2 = new(intf.MON, intf.DRV, intf.REF);
 
 
-   // tb3 = new(intf.MON, intf.DRV, intf.REF);
+    tb3 = new(intf.MON, intf.DRV, intf.REF);
 
 
-   // tb4 = new(intf.MON, intf.DRV, intf.REF);
+    tb4 = new(intf.MON, intf.DRV, intf.REF);
 
 
     tb_regression = new(intf.MON, intf.DRV, intf.REF);
@@ -112,7 +74,7 @@ module top();
 
     tb_regression.run();
 
-/*   tb.run();
+  tb.run();
 
     tb1.run();
 
@@ -120,7 +82,7 @@ module top();
 
     tb3.run();
 
-    tb4.run();*/
+    tb4.run();
 
     #2000;
 
